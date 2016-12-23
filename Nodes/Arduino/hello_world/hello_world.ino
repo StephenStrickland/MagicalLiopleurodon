d2c26462967e7364c00c188e8a66ca863d92b9c2
Stephen Strickland <stephen@startup-rescue.com>
2016-12-22 22:07:32 -0500

    includes hello world arduino file.

---
+++ Nodes/Arduino/hello_world/hello_world.ino
@@ -0,0 +1,14 @@
+//just blinks the LED on the MEGA board
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(200);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(200);         
+}
