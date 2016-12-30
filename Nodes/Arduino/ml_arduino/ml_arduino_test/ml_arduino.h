aa60d7693d225b347ccffd09f41afabbe03e9d0d
Stephen Strickland <stephen@startup-rescue.com>
2016-12-29 19:42:34 -0500

    add init library files

---
+++ Nodes/Arduino/ml_arduino/ml_arduino_test/ml_arduino.h
@@ -0,0 +1,13 @@
+/*
+ * MagicalLiopleurodon is a great library to protext your securities
+ * Created by Stephen M. Strickland and Chris A. Koronkowski, (as if you couldn't guess) December 29, 2016
+ * Released in to the not so public domain.
+ */
+
+#ifndef  MagicalLiopleurodon_h
+#define  MagicalLiopleurodon_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include "Arduino.h"
+
+#endif
---
+++ Nodes/Arduino/ml_arduino/ml_arduino_test/ml_arduino_test.ino
@@ -0,0 +1,12 @@
+#include <Printers.h>
+#include <XBee.h>
+XBee xbee = XBee();
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
