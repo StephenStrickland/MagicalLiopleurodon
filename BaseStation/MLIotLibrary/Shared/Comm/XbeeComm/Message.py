002ce3fa2e31ede0c6003fbd211743948d1143e7
DESKTOP-RFAOA5K\cakor <cakoronkowski@gmail.com>
2017-04-06 16:43:35 -0400

    i really hope this doesnt break anything

--- BaseStation/MLIotLibrary/Shared/Comm/XbeeComm/Message.py
+++ BaseStation/MLIotLibrary/Shared/Comm/XbeeComm/Message.py
@@ -1,13 +1,16 @@
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
+import json
+from ....Shared.Crypto import Crypt
 
 class Message:
-    def __init__(self, dataIv, dataString, dataSignedHash, senderId, nonce):
+    def __init__(self, dataIv, dataString, dataSignedHash, senderId, recipientList, nonce):
         self.iv=dataIv
-        self.data=dataString
+        self.data={"dataString":dataString, "nonce":nonce}
+        self.encData
         self.signature=dataSignedHash
         self.sender=senderId
-        self.nonce=nonce
+        self.recipients=recipientList
 
     def validate(self):
         return True
@@ -15,12 +18,18 @@ class Message:
     def getDict(self):
         return {
             'i': self.iv,
-            'd': self.data,
+            'd': self.encData,
             'sg': self.signature,
             's': self.sender,
-            'n': self.nonce
+            'r': self.recipients
 
         }
+    def prepMessage(self, recipient):
+        self.encData=Crypt.CryptoManager.encryptMessage(json.dumps(self.data),recipient)
+
+    def processMessage(self):
+        self.data=json.loads(Crypt.CryptoManager.decryptMessage(self.encData,self.iv,self.sender))
+
         
 
 
