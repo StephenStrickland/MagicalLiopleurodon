eacffbbaff5d7769246ebf7a43fdcfd45d19f25b
Stephen Strickland <stephen@startup-rescue.com>
2017-04-26 02:17:44 -0400

    handles at command requests

--- Nodes/Fixes/src/main.cpp
+++ Nodes/Fixes/src/main.cpp
@@ -5,8 +5,11 @@
  * then off for one second, repeatedly.
  */
 #include "Arduino.h"
-#include <ArduinoJson.h>
 #include "EEPROM.h"
+#include <ArduinoJson.h>
+#include <Printers.h>
+#include <XBee.h>
+
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13
@@ -14,6 +17,8 @@
 
 
 #define CONFIG_TRIGGER_PIN 5
+
+XBee xbee = XBee();
 
 /** Struct of the JSON config file
  *
@@ -87,11 +92,97 @@ void sendATCommand()
 
 }
 
+
+/** sends AT Command to XBee radio
+ *
+ */
+void sendAtCommand(AtCommandRequest req)
+{
+  Serial.println("Sending command to the XBee");
+  AtCommandResponse atResponse = AtCommandResponse();
+  // send the command
+  xbee.send(req);
+
+  // wait up to 5 seconds for the status response
+  if (xbee.readPacket(5000)) {
+    // got a response!
+
+    // should be an AT command response
+    if (xbee.getResponse().getApiId() == AT_COMMAND_RESPONSE) {
+      xbee.getResponse().getAtCommandResponse(atResponse);
+
+      if (atResponse.isOk()) {
+        Serial.print("Command [");
+        Serial.print(atResponse.getCommand()[0]);
+        Serial.print(atResponse.getCommand()[1]);
+        Serial.println("] was successful!");
+
+        if (atResponse.getValueLength() > 0) {
+          Serial.print("Command value length is ");
+          Serial.println(atResponse.getValueLength(), DEC);
+
+          Serial.print("Command value: ");
+
+          for (int i = 0; i < atResponse.getValueLength(); i++) {
+            Serial.print(atResponse.getValue()[i], HEX);
+            Serial.print(" ");
+          }
+
+          Serial.println("");
+        }
+      }
+      else {
+        Serial.print("Command return error code: ");
+        Serial.println(atResponse.getStatus(), HEX);
+      }
+    } else {
+      Serial.print("Expected AT response but got ");
+      Serial.print(xbee.getResponse().getApiId(), HEX);
+    }
+  } else {
+    // at command failed
+    if (xbee.getResponse().isError()) {
+      Serial.print("Error reading packet.  Error code: ");
+      Serial.println(xbee.getResponse().getErrorCode());
+    }
+    else {
+      Serial.print("No response from radio");
+    }
+  }
+}
+
 /** configures XBee radio
  * sets up radio level encryption
  */
 void configureXBee()
 {
+  //Encryption Enable
+  uint8_t eeCmd[] = {'E', 'E'};
+  //Turn on EE
+  uint8_t eeVal[] = {1};
+  //Encryption Key
+  uint8_t kyCmd[] = {'K', 'Y'};
+  //Write commands to non-volatile memory
+  uint8_t wrCmd[] = {'W', 'R'};
+  //Default key, for now.
+  uint8_t key[] = {'t', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'k', 'e', 'y', '1', '2', '3'};
+
+  AtCommandRequest atRequest = AtCommandRequest();
+  delay(5000);
+  atRequest.setCommand(eeCmd);
+  atRequest.setCommandValue(eeVal);
+  atRequest.setCommandValueLength(1);
+  sendAtCommand(atRequest);
+  atRequest.clearCommandValue();
+  atRequest.setCommand(kyCmd);
+  atRequest.setCommandValue(key);
+  atRequest.setCommandValueLength(16);
+  sendAtCommand(atRequest);
+  atRequest.clearCommandValue();
+
+  atRequest.setCommand(wrCmd);
+  sendAtCommand(atRequest);
+//  delete atRequest;
 
 }
 
